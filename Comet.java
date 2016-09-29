1660bc21cfe18604a4d18ebe5419599f5d8a1659
Ben Gyori <ben.gyori@gmail.com>
2016-09-29 16:37:03 -0700

    Cosmetic changes

--- Comet.java
+++ Comet.java
@@ -29,95 +29,95 @@
 import ij.gui.Roi;
 
 public class Comet {
-	public Comet(Roi roi){
-		this.cometRoi = (Roi)roi.clone();
-		this.status = Comet.VALID;
-	} 
-	
-	public int id;
-	public int status;
-	public static final int VALID = 1;
-	public static final int INVALID = 2;
-	public static final int OUTLIER = 4;
-	public static final int DELETED = 8;
-	
-	public Roi cometRoi;
-	public Roi headRoi;
-	public Roi oldRoi;
-	
-	// Position in image
-	public int x,y, height, width;
-	
-	// Profiles
-	public double[] cometProfile;
-	public double[] bgProfile;
-	public double[] headProfile;
-	public double[] tailProfile;
-	public double profileMax;
-	
-	// Output measurements
-	public double cometArea;
-	public double cometIntensity;
-	public double cometLength;
-	public double cometDNA;
-	public double headArea;
-	public double headIntensity;
-	public double headLength;
-	public double headDNA;
-	public double headDNAPercent;
-	public int headCentroid;
-	public double tailArea;
-	public double tailLength;
-	public double tailIntensity;
-	public double tailDNA;
-	public double tailDNApercent;
-	public int tailCentroid;
-	public double tailMoment;
-	public double tailOliveMoment;
-	
-	// Internal parameters
-	public int[] histogram;
-	public double area;
-	public double areaConvexHull;
-	public double mean;
-	public double convexity;
-	public double symmetry;
-	public double perimeter;
-	public double circularity;
-	public double centerlineDiff;
-	public double hratio;
-	public int headFrontCenterY; // are these needed?
-	public int headRoiCenterY;
-	
-	public String getMeasurementString(String sep){
-		String flagStr;
-		if(status==VALID)
-			flagStr = "normal";
-		else if (status==OUTLIER)
-			flagStr = "outlier";
-		else if (status==DELETED)
-			flagStr = "deleted";
-		else
-			return "";
-		
-		String result = id + sep
-				+ flagStr + sep 
-				+ cometArea + sep
-				+ cometIntensity + sep
-				+ cometLength + sep 
-				+ cometDNA + sep 
-				+ headArea + sep 
-				+ headIntensity + sep 
-				+ headLength + sep 
-				+ headDNA + sep 
-				+ headDNAPercent + sep
-				+ tailArea + sep 
-				+ tailIntensity + sep 
-				+ tailLength + sep
-				+ tailDNA + sep 
-				+ tailDNApercent + sep
-				+ tailMoment + sep
-				+ tailOliveMoment;
-		return result;
-		}
+    public Comet(Roi roi){
+        this.cometRoi = (Roi)roi.clone();
+        this.status = Comet.VALID;
+    } 
+
+    public int id;
+    public int status;
+    public static final int VALID = 1;
+    public static final int INVALID = 2;
+    public static final int OUTLIER = 4;
+    public static final int DELETED = 8;
+
+    public Roi cometRoi;
+    public Roi headRoi;
+    public Roi oldRoi;
+
+    // Position in image
+    public int x,y, height, width;
+
+    // Profiles
+    public double[] cometProfile;
+    public double[] bgProfile;
+    public double[] headProfile;
+    public double[] tailProfile;
+    public double profileMax;
+
+    // Output measurements
+    public double cometArea;
+    public double cometIntensity;
+    public double cometLength;
+    public double cometDNA;
+    public double headArea;
+    public double headIntensity;
+    public double headLength;
+    public double headDNA;
+    public double headDNAPercent;
+    public int headCentroid;
+    public double tailArea;
+    public double tailLength;
+    public double tailIntensity;
+    public double tailDNA;
+    public double tailDNApercent;
+    public int tailCentroid;
+    public double tailMoment;
+    public double tailOliveMoment;
+
+    // Internal parameters
+    public int[] histogram;
+    public double area;
+    public double areaConvexHull;
+    public double mean;
+    public double convexity;
+    public double symmetry;
+    public double perimeter;
+    public double circularity;
+    public double centerlineDiff;
+    public double hratio;
+    public int headFrontCenterY; // are these needed?
+    public int headRoiCenterY;
+
+    public String getMeasurementString(String sep){
+        String flagStr;
+        if(status==VALID)
+            flagStr = "normal";
+        else if (status==OUTLIER)
+            flagStr = "outlier";
+        else if (status==DELETED)
+            flagStr = "deleted";
+        else
+            return "";
+
+        String result = id + sep
+                + flagStr + sep 
+                + cometArea + sep
+                + cometIntensity + sep
+                + cometLength + sep 
+                + cometDNA + sep 
+                + headArea + sep 
+                + headIntensity + sep 
+                + headLength + sep 
+                + headDNA + sep 
+                + headDNAPercent + sep
+                + tailArea + sep 
+                + tailIntensity + sep 
+                + tailLength + sep
+                + tailDNA + sep 
+                + tailDNApercent + sep
+                + tailMoment + sep
+                + tailOliveMoment;
+        return result;
+        }
 }
